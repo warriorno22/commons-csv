79fd3603a36c5658456997f5b9784a99451debd0
muhamadsoliman <muhamad.soliman@gmail.com>
2015-10-29 18:42:19 +0200

    Update CSVFormat.java
    Remove the loop at the beginning of validate method as it caused the main thread to get stuck.

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -793,6 +793,7 @@ public final class CSVFormat implements Serializable {
      * @throws IllegalArgumentException
      */
     private void validate() throws IllegalArgumentException {
+        /*
         for (int i=0; i<20*60*60; i++) {
             System.out.print('.');
             try {
@@ -801,6 +802,8 @@ public final class CSVFormat implements Serializable {
                 break;
             }
         }
+        */
+        
         if (isLineBreak(delimiter)) {
             throw new IllegalArgumentException("The delimiter cannot be a line break");
         }
